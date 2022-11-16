f432893dae15d1353895cac3b1bd07d113a7e1c1
fhossain <farhana.hossainse@gmail.com>
2022-11-16 15:16:46 -0500

    env file added

--- fmazon/fmazon/settings.py
+++ fmazon/fmazon/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+4*%o5v0(9q_0+a+7jce!o0l5vo3+_ql65n5x^bh5a07*kf+$e'
+
+SECRET_KEY = env('SECRET_KEY')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
