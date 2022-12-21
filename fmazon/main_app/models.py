c3f305746f4e805d6bd39c23087773500662342e
fhossain <farhana.hossainse@gmail.com>
2022-12-20 20:14:45 -0500

    added models

--- fmazon/main_app/models.py
+++ fmazon/main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    name: models.CharField(max_length=50)
+    quantity: models.IntegerField()
+    price: models.IntegerField()
+    description: models.CharField()
+
+    def _str_(self):
+        return self.name
+
+class Review(models.Model):
+    item= models.ForeignKey(Item, on_delete=models.CASCADE, related_name='reviews')
+    title: models.CharField(max_length=50)
+    comment: models.CharField(max_length=300)
+
+    def _str_(self):
+        return self.title
--- fmazon/main_app/views.py
+++ fmazon/main_app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
+class Item:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, quantity, description):
+    self.name = name
+    self.quantity = quantity
+    self.description = description
+
+
+items = [
+  Item('Lolo', 2, 'foul little demon'),
+  Item('Sachi', 3, 'diluted tortoise shell'),
+  Item('Raven', 4, '3 legged cat')
+]
+
 def home(request):
-    return HttpResponse('<h1>Hello Friends!</h1>')
+    return HttpResponse('<h1>Hello Friends!< /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def items_index(request):
+    return render(request, 'items/items.html', {'items': items})
